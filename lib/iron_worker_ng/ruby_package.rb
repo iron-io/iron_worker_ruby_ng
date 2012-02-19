96dd4d348f90c3ee46f494f74b978bbd676f8b5d
Andrew Kirilenko <andrew.kirilenko@gmail.com>
2012-02-19 08:31:08 +0300

    Class-less workers support for Ruby.

--- lib/iron_worker_ng/ruby_package.rb
+++ lib/iron_worker_ng/ruby_package.rb
@@ -40,24 +40,37 @@ require 'json'
 
 payload = JSON.load(File.open(payload_file))
 
+@iron_io_project_id = payload['project_id']
+@iron_io_token = payload['token']
+@iron_worker_task_id = task_id
+@params = payload['params']
+
 require worker_file_name
 
-worker_class = Kernel.const_get(worker_class_name)
-worker_inst = worker_class.new
+worker_class = nil
 
-class << worker_inst
-  attr_accessor :iron_io_project_id
-  attr_accessor :iron_io_token
-  attr_accessor :iron_worker_task_id
-  attr_accessor :params
+begin
+  worker_class = Kernel.const_get(worker_class_name)
+rescue
 end
 
-worker_inst.iron_io_project_id = payload['project_id']
-worker_inst.iron_io_token = payload['token']
-worker_inst.iron_worker_task_id = task_id
-worker_inst.params = payload['params']
+unless worker_class.nil?
+  worker_inst = worker_class.new
 
-worker_inst.run
+  class << worker_inst
+    attr_accessor :iron_io_project_id
+    attr_accessor :iron_io_token
+    attr_accessor :iron_worker_task_id
+    attr_accessor :params
+  end
+
+  worker_inst.iron_io_project_id = @iron_io_project_id
+  worker_inst.iron_io_token = @iron_io_token
+  worker_inst.iron_worker_task_id = @iron_worker_task_id
+  worker_inst.params = @params
+
+  worker_inst.run
+end
 RUNNER
       end
     end
