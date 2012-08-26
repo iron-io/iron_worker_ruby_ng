201888ad55e8c51231066c8106859f13acff8114
Travis Reeder <treeder@gmail.com>
2012-08-25 18:30:02 -0700

    Waits for all retried tasks to complete.

--- test/tmp.rb
+++ test/tmp.rb
@@ -11,9 +11,14 @@ class QuickRun < IWNGTest
 
   def test_auto_retry
     name = 'test_auto_retry'
+
+    retries = 3
+    retries_delay = 5
+
     client.codes.create(code_bundle(:name => name,
-                                    :exec => 'workers/fail_worker.rb'
-                        ), :num_retries => 3)
+                                    :exec => 'workers/fail_worker.rb'),
+                        :retries => retries,
+                        :retries_delay => retries_delay)
 
     task_ids = []
     N_TASKS.times do
@@ -21,15 +26,22 @@ class QuickRun < IWNGTest
     end
 
     task_ids.each do |id|
-      task = client.tasks.wait_for(id)
-      p task
-      puts "retry_num: #{task.retry}"
-      puts "original_task_id: #{task.original_task_id}"
-      puts "retry_task_id: #{task.retry_task_id}"
-      assert_equal 'error', task.status
-      log = client.tasks.log(id)
-      assert log.include?("Fail Whale")
-      assert task.retry_task_id
+      j = 0
+      tid = id
+      while tid != nil
+        task = client.tasks.wait_for(tid)
+        p task
+        puts "retry_num: #{task.retry}"
+        puts "original_task_id: #{task.original_task_id}"
+        puts "retry_task_id: #{task.retry_task_id}"
+        assert_equal 'error', task.status
+        log = client.tasks.log(id)
+        assert log.include?("Fail Whale")
+        assert task.retry_task_id if j < retries
+        tid = task.retry_task_id
+        j += 1
+
+      end
 
     end
   end
