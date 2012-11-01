0819210e17d477fe4174c2e0d0d48d06b286e132
Andrew Kirilenko <andrew.kirilenko@gmail.com>
2012-11-01 03:50:25 +0300

    Trying to fix yet another bunlder bug (path reporting for self).

--- lib/iron_worker_ng/feature/ruby/merge_gem.rb
+++ lib/iron_worker_ng/feature/ruby/merge_gem.rb
@@ -36,17 +36,27 @@ module IronWorkerNG
               if @spec.extensions.length == 0 || IronWorkerNG::Feature::Ruby::MergeGem.merge_binary?
                 IronCore::Logger.debug 'IronWorkerNG', "Bundling ruby gem with name='#{@spec.name}' and version='#{@spec.version}'"
 
+                full_gem_path = @spec.full_gem_path                
+                loaded_from = @spec.loaded_from
+
+                # yet another bundler fix
+
+                if loaded_from.end_with?("/gems/bundler-#{@spec.version}/lib/bundler")
+                  full_gem_path = gem_path
+                  loaded_from = loaded_from.gsub("/gems/bundler-#{@spec.version}/lib/bundler", "/specifications/bundler-#{@spec.version}.gemspec")
+                end
+
                 if File.exists?(@spec.full_gem_path)
                   container_add(container, '__gems__/gems/' + @spec.full_name, gem_path)
                 else
-                  from_dir = File.dirname(@spec.loaded_from)
+                  from_dir = File.dirname(loaded_from)
 
                   @spec.files.each do |fname|
                     container_add(container, '__gems__/gems/' + @spec.full_name + '/' + fname, from_dir + '/' + fname) if File.file?(from_dir + '/' + fname)
                   end
                 end
 
-                container_add(container, "__gems__/specifications/#{@spec.full_name}.gemspec", @spec.loaded_from)
+                container_add(container, "__gems__/specifications/#{@spec.full_name}.gemspec", loaded_from)
               else
                 IronCore::Logger.warn 'IronWorkerNG', "Skipping ruby gem with name='#{@spec.name}' and version='#{@spec.version}' as it contains native extensions, switching to full remote build should fix this"
               end
--- lib/iron_worker_ng/version.rb
+++ lib/iron_worker_ng/version.rb
@@ -1,5 +1,5 @@
 module IronWorkerNG
-  VERSION = '0.12.1'
+  VERSION = '0.12.2'
 
   def self.version
     VERSION
