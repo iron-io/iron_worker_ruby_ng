d0c475a7e71d89b20c4f00ea619753680d5f429b
Roman Dzvinkovsky <romand@iron.io>
2012-12-14 06:37:16 +0700

    Squashed 'examples/' changes from d57939e..c167210
    c167210 (split)updating actionmailer_standalone example
    
    git-subtree-dir: examples
    git-subtree-split: c167210750beaaf20e64753a7f6ca9d94c5e5f5e

--- actionmailer_standalone/enqueue.rb
+++
@@ -1,8 +0,0 @@
-require 'iron_worker_ng'
-require_relative '../../ruby/examples_helper'
-
-config = ExamplesHelper.load_config
-
-client = IronWorkerNG::Client.new(:token => config['iw']['token'], :project_id => config['iw']['project_id'])
-
-client.tasks.create('ActionmailerWorker', :gmail => config['gmail'], :from => 'andrew@iron.io', :to => ['andrew@iron.io']) # I like receiving mails
