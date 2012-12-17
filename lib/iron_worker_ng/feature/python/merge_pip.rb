cdeba15217b5b6a96a2d941a6e1eb6507186c183
thousandsofthem <thousandsofthem@gmail.com>
2012-12-17 14:56:24 +0300

    automatic directory location search

--- lib/iron_worker_ng/feature/python/merge_pip.rb
+++ lib/iron_worker_ng/feature/python/merge_pip.rb
@@ -24,12 +24,12 @@ module IronWorkerNG
             deps_string = @deps.map { |dep| dep.version == '' ? dep.name : dep.name + '==' + dep.version }.join(' ')
             install_command = 'pip install --upgrade --install-option="--prefix=' + tmp_dir_name + '" ' + deps_string
 
-            packages_dir = tmp_dir_name + '/lib/python2.7/site-packages'
-
-            ::FileUtils.mkdir_p tmp_dir_name + '/bin'
-            ::FileUtils.mkdir_p packages_dir
+            ::Dir.mkdir tmp_dir_name + '/bin'
+            ::Dir.mkdir tmp_dir_name + '/lib'
 
             system(install_command)
+
+            packages_dir = Dir.glob(tmp_dir_name + '/lib/*python*').first + '/site-packages'
 
             container_add(container, '__pips__/bin', tmp_dir_name + '/bin', true)
             container_add(container, '__pips__', packages_dir , true)
