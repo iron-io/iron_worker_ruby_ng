850535a12ac490cb0bd9516c3f785e8a90d39a7e
thousandsofthem <thousandsofthem@gmail.com>
2013-08-29 10:43:19 +0300

    add runner for node runtime

--- lib/iron_worker_ng/code/runtime/node.rb
+++ lib/iron_worker_ng/code/runtime/node.rb
@@ -4,6 +4,32 @@ module IronWorkerNG
       module Node
         include IronWorkerNG::Feature::Common::MergeExec::InstanceMethods
 
+        def runtime_bundle(container, local = false)
+          container.get_output_stream(@dest_dir + 'node_modules/node_helper.js') do |runner|
+            runner.write <<NODE_RUNNER
+/* #{IronWorkerNG.full_version} */
+
+var fs = require('fs');
+var params = null;
+var task_id = null;
+
+process.argv.forEach(function(val, index, array) {
+  if (val == "-payload" && index < process.argv.length) {
+    params = JSON.parse(fs.readFileSync(process.argv[index + 1], 'ascii'));
+  }
+
+  if (val == "-id" && index < process.argv.length) {
+    task_id = process.argv[index + 1];
+  }
+});
+
+exports.params = params;
+exports.task_id = task_id;
+
+NODE_RUNNER
+          end
+        end
+
         def runtime_run_code(local = false)
           <<RUN_CODE
 node #{File.basename(@exec.path)} "$@"
