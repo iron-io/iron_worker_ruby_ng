b4aa3587dbb07cbb0bc47d7fcac5698ec253e578
rkononov <rkononov@gmail.com>
2014-03-28 17:44:12 +0600

    added tests python stack

---
+++ test/stacks/python-2.7/python2.py
@@ -0,0 +1,3 @@
+import sys
+if sys.version_info[0] == 2 & sys.version_info[1] == 7:
+    raise "Must be using Python 2"
---
+++ test/stacks/python-3.2/python3.py
@@ -0,0 +1,3 @@
+import sys
+if sys.version_info[0] == 3 & sys.version_info[1] == 2:
+    raise "Must be using Python 3"
