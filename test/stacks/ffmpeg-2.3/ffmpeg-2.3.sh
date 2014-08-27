47bc269c51c1b774548388719f2a21902402984f
freeman <kononov.dmitriy@gmail.com>
2014-08-27 17:41:10 +0600

    adding tests for php-5.5 and fixing  test for ffmpeg-2.3

--- test/stacks/ffmpeg-2.3/ffmpeg-2.3.sh
+++ test/stacks/ffmpeg-2.3/ffmpeg-2.3.sh
@@ -1,8 +1,10 @@
-correct_version_mp4box="0.5.1-DEV-rev4065"
-correct_version_ffmpeg="0.8.10-6"
+correct_version_mp4box="0.5.1-DEV-rev5372"
+correct_version_ffmpeg="2.3"
+correct_version_node="v0.10.29"
 
 current_version_mp4box=`MP4Box -version 2>&1 | head -n 1 | awk -F 'version ' '{print $2}'`
-current_version_ffmpeg=`ffmpeg -version 2>&1 | head -n 1 | awk -F 'version ' '{print $2}' | head -c8`
+current_version_ffmpeg=`ffmpeg -version 2>&1 | head -n 1 | awk -F 'version ' '{print $2}' | head -c3`
+current_version_node=`node -v`
 
 if [ "$current_version_mp4box" = "$correct_version_mp4box" ]; then
     echo "Version of mp4box is correct. $correct_version_mp4box"
@@ -18,4 +20,10 @@ else
     exit 1
 fi
 
+if [ "$current_version_node" = "$correct_version_node" ]; then
+    echo "Version of node is correct. $correct_version_node"
+else
+    echo "Version of node is incorrect. $current_version_node"
+    exit 1
+fi
 exit 0
