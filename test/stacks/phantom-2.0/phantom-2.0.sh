f6eb09f8054e78707494a3fbc09ada569ef0cec4
Ulan Djamanbalaev <udjamanbalaev@gmail.com>
2015-01-23 12:41:14 +0600

    checked phantomjs' versions

--- test/stacks/phantom-2.0/phantom-2.0.sh
+++ test/stacks/phantom-2.0/phantom-2.0.sh
@@ -1,1 +1,10 @@
-phantomjs -v
+correct_version="2.0.0"
+current_version=`phantomjs -v 2>&1 | head -n 1 | awk -F ' ' '{print $1}' | head -c6`
+
+if [ "$current_version" = "$correct_version" ]; then
+    echo "Version of Phantomjs is correct. $correct_version"
+    exit 0
+else
+    echo "Version of Phantomjs is incorrect. $current_version=$correct_version"
+    exit 1
+fi
