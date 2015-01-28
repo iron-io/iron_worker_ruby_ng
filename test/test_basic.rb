c36fa7f7bc8a3b1f2973476037591fda6afd9f76
sunloverz <sunloverz.kg@gmail.com>
2015-01-28 13:07:33 +0600

    Cover with tests all new IronWorker features (label,tasks on pause)

--- test/test_basic.rb
+++ test/test_basic.rb
@@ -2,12 +2,7 @@ require_relative 'helpers'
 
 class BasicTest < IWNGTest
   def test_basic
-    code = IronWorkerNG::Code::Base.new do
-      name 'test_basic'
-      exec(File.dirname(__FILE__) + '/hello.rb')
-    end
-
-    client.codes_create(code)
+    create_code('test_basic')
     task_id = client.tasks_create('test_basic').id
 
     task = client.tasks_wait_for(task_id)
@@ -19,5 +14,45 @@ class BasicTest < IWNGTest
 
     log = client.tasks_log(task_id)
     assert_equal( "hello\n", log, "worker stdout is in log" )
+  end
+
+  def test_task_label
+    create_code('test_label')
+    resp = client.tasks.create('test_label', {}, label: 'new_label' )
+    task = client.tasks.get(resp.id)
+    assert_equal "new_label", task.label
+  end
+
+  def test_schedule_label
+    create_code('test_label')
+    resp = client.schedules.create('test_label', {}, start_at: Time.now + 120, run_times: 1, label: 'schedule_label' )
+    task = client.schedules.get(resp.id)
+    assert_equal "schedule_label", task.label
+  end
+
+  def test_pause_task_queue
+    code_id = create_code('test_paused_task')
+    client.tasks.create('test_paused_task')
+    response = client.codes.pause_task_queue(code_id)
+    paused_code = client.codes.get(code_id)
+    assert_equal response['msg'], 'Paused'
+    assert_equal paused_code.max_concurrency, -1
+    client.codes.resume_task_queue(code_id)
+  end
+
+  def test_resume_task_queue
+    code_id = create_code('test_resumed_task')
+    client.tasks.create('test_paused_task')
+    client.codes.pause_task_queue(code_id)
+    sleep 1
+    response = client.codes.resume_task_queue(code_id)
+    resumed_code = client.codes.get(code_id)
+    assert_equal response['msg'], 'Resumed'
+    assert_not_equal resumed_code.max_concurrency, -1
+  end
+
+  def create_code(name)
+    code = code_bundle(exec: 'test/hello.rb', name: name)
+    client.codes.create(code).id
   end
 end
