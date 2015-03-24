c21f60071a99465c302c517e1c7745af29838ceb
sunloverz <sunloverz.kg@gmail.com>
2015-03-24 15:29:08 +0600

    Refactor merge requirements

--- lib/iron_worker_ng/feature/python/merge_requirements.rb
+++ lib/iron_worker_ng/feature/python/merge_requirements.rb
@@ -2,19 +2,9 @@ module IronWorkerNG
   module Feature
     module Python
       module MergeRequirements
-        class Feature < IronWorkerNG::Feature::Base
-          attr_reader :path
-
-          def initialize(code, path)
-            super(code)
-
-            @path = path
-          end
-        end
-
         module InstanceMethods
           def merge_requirements(path)
-            feature = IronWorkerNG::Feature::Python::MergeRequirements::Feature.new(self, path)
+            feature = IronWorkerNG::Feature::Base.new(self)
             IronWorkerNG::Fetcher.fetch_to_file(feature.rebase(path)) do |requirements|
               specs = []
               File.readlines(requirements).map { |line| specs << line.split('==')}
