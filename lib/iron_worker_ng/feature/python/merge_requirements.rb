bc31124b2a5701ed1cccc234fa5b4f0c0fe4dc03
sunloverz <sunloverz.kg@gmail.com>
2015-03-26 16:12:14 +0600

    Parse requirements.txt and skip blank lines, comments

--- lib/iron_worker_ng/feature/python/merge_requirements.rb
+++ lib/iron_worker_ng/feature/python/merge_requirements.rb
@@ -3,17 +3,30 @@ module IronWorkerNG
     module Python
       module MergeRequirements
         module InstanceMethods
+
           def merge_requirements(path)
             feature = IronWorkerNG::Feature::Base.new(self)
             IronWorkerNG::Fetcher.fetch_to_file(feature.rebase(path)) do |requirements|
-              specs = []
-              File.readlines(requirements).map { |line| specs << line.split('==')}
-              specs.each {|spec| merge_pip(spec[0], spec[1])}
+              specs = parse_requirements(requirements)
+              specs.each {|spec| merge_pip(spec[:name], spec[:version])}
             end
             @features << feature
           end
 
           alias :requirements :merge_requirements
+
+          private
+          def parse_requirements(file)
+            specs = []
+            File.readlines(file).each do |line|
+              line = line.strip
+              next if line.to_s.empty? || line.start_with?('#')
+              spec = line.split(/(==|>=|<=|<|>)/, 2)
+              version = spec[1]? spec[1]+spec[2]: ''
+              specs << {name: spec[0], version:version}
+            end
+            specs
+          end
         end
       end
     end
